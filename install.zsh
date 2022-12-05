54e30b372ce9da3e5961b430a4881b95b7b32745
KH <ganghe74@gmail.com>
2022-12-05 18:24:20 +0900

    install.zsh: resolve some issues
    resolve some issues
    disable fast repo server, it's already fast. maybe aistages have cache.
    
    and dealing with annoying locale setting
    
    Issues: #17 #18

--- install.zsh
+++ install.zsh
@@ -19,12 +19,16 @@ fi
 SUDO=''
 if (( $EUID != 0 )); then
   SUDO='sudo'
+  SUDOE='sudo -E'
 fi
 
 # fast repo server
-$SUDO cp /etc/apt/sources.list /etc/apt/sources.list.bak
-$SUDO sed -i 's|/archive.ubuntu|/mirror.kakao|g' /etc/apt/sources.list
-$SUDO sed -i 's|/security.ubuntu|/mirror.kakao|g' /etc/apt/sources.list
+# $SUDO cp /etc/apt/sources.list /etc/apt/sources.list.bak
+# $SUDO sed -i 's|/archive.ubuntu|/mirror.kakao|g' /etc/apt/sources.list
+# $SUDO sed -i 's|/security.ubuntu|/mirror.kakao|g' /etc/apt/sources.list
+
+# locale
+$SUDO locale-gen en_US.UTF-8
 
 # default
 $SUDO apt update
@@ -36,8 +40,8 @@ $SUDO apt update
 $SUDO apt install -y neovim
 
 # nodejs
-curl -fsSL https://deb.nodesource.com/setup_16.x | $SUDO -E bash - &&\
-$SUDO apt-get install -y nodejs
+curl -fsSL https://deb.nodesource.com/setup_16.x | $SUDE bash - &&\
+$SUDO apt install -y nodejs
 $SUDO npm i -g serve diff-so-fancy
 
 # debs
@@ -63,6 +67,6 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # symlink
 mv ~/.bashrc ~/.bashrc.bak
 mv ~/.zshrc ~/.zshrc.bak
-stow bash bin git ssh vim zsh
+stow bash bin git ssh vim zsh tmux
 
 exec zsh
