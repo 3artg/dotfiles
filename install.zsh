2d48cbc85b6490911f72501a8d5d340af66d26de
KH <ganghe74@gmail.com>
2022-12-17 23:45:06 +0900

    install.zsh: substitute `RUNZSH=no` for `--unattended`

--- install.zsh
+++ install.zsh
@@ -50,8 +50,7 @@ $SUDO dpkg -i tmp/*.deb
 rm -rf tmp
 
 # oh-my-zsh
-RUNZSH=no
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
